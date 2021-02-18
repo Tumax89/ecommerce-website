a37787b047467cf62cdc9cbe3d328c7fbff63008
Tumax89 <58259163+Tumax89@users.noreply.github.com>
2021-02-19 00:37:11 +0800

    Add Redux to Homescreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
