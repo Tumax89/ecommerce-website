44fa9e03bafc3f942e4c41d75bdca921b1f25014
Tumax89 <58259163+Tumax89@users.noreply.github.com>
2021-02-20 00:42:18 +0800

    Add redux to product screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,15 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducer";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducer";
 const initialState = {};
-const reducer = combineReducers({ productList: productListReducer });
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 const composeEnchancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
