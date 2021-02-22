339ae76c7b66c9fa95eb196ec90a3367a218d195
Tumax89 <58259163+Tumax89@users.noreply.github.com>
2021-02-23 00:11:21 +0800

    Part 18 Create Sample Users in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,39 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const MONGODB_URL =
+  "mongodb+srv://ecommerce123:ecommerce123@ecommerce-website.utf7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URL || "mongodb://localhost/ecommerce-website", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find((x) => x._id === productId);
   if (product) res.send(product);
   else res.status(404).send({ msg: "Product Not Found" });
 });
+
+app.use("/api/users", userRouter);
+
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-const port = process.env.PORT || 5000;
-app.listen(port, () =>
-  console.log(`server started at htpp://localhost:${port}`)
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`server started at htpp://localhost:${PORT}`)
 );
